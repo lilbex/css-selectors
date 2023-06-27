afb62de43f453a6856bd4a3e138a4171165eb2da
lilbex <47287618+lilbex@users.noreply.github.com>
2023-06-27 13:54:59 +0100

    added assignment

---
+++ onramp-onsite/assignment.md
@@ -0,0 +1,38 @@
+Assignment 1
+
+
+Write a function that adds multiple of 3 and 5
+
+Assignment 2
+
+Create a function that returns the sum of the two lowest positive numbers given an array of minimum 4 positive integers. No floats or non-positive integers will be passed.
+
+
+
+
+For example, when an array is passed like [19, 5, 42, 2, 77], the output should be 7.
+
+
+
+
+[10, 343445353, 3453445, 3453545353453] should return 3453455.
+
+
+
+
+
+
+
+Assignment 3
+
+You are required to, given a string, replace every letter with its position in the alphabet.
+
+If anything in the text isn't a letter, ignore it and don't return it.
+
+"a" = 1, "b" = 2, etc.
+
+Example
+
+alphabet_position("The sunset sets at twelve o' clock.")
+
+Should return "20 8 5 19 21 14 19 5 20 19 5 20 19 1 20 20 23 5 12 22 5 15 3 12 15 3 11" ( as a string )
--- onramp-onsite/object-tut.js
+++ onramp-onsite/object-tut.js
@@ -51,4 +51,50 @@ console.log(firstNames);
 // the array.  If there are no numbers, return 0
 
 const arr = [1, 2, 3, "4", "5", 1, "men", "8", "9", 4];
-function addNumbers() {}
+function addNumbers(arr) {
+  let sum = 0;
+  for (let i = 0; i < arr.length; i++) {
+    if (typeof arr[i] === "number") {
+      sum += arr[i];
+    }
+  }
+  return sum;
+}
+// using for in loop
+function addNumbers2(arr) {
+  let sum = 0;
+  for (let i in arr) {
+    if (typeof arr[i] === "number") {
+      sum += arr[i];
+    }
+  }
+  return sum;
+}
+
+// Assignment 1
+const sumOfMultiples = (num) => {
+  let sum = 0;
+  for (let i = 0; i < num; i++) {
+    if (i % 3 === 0 || i % 5 === 0) {
+      sum += i;
+    }
+  }
+  return sum;
+}
+// Assignment 2
+const sumOfTwoSmallestNums = (arr) => {
+  let sorted = arr.sort((a, b) => a - b);
+  return sorted[0] + sorted[1];
+}
+// Assignment 3
+const letters = "abcdefghijklmnopqrstuvwxyz";
+const alphabetPosition = (text) => {
+  let result = [];
+  for (let i = 0; i < text.length; i++) {
+    let letter = text[i].toLowerCase();
+    if (letters.includes(letter)) {
+      result.push(letters.indexOf(letter) + 1);
+    }
+  }
+  return result.join(" ");
+}
